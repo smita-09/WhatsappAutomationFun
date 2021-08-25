6b90ae7680e347659a8cb342b6d4d371186571df
smita-09 <smita09021996@gmail.com>
2021-08-25 13:51:31 +0200

    starter file

---
+++ starter.py
@@ -0,0 +1,3 @@
+import pywhatkit
+# pywhatkit.sendwhatmsg('+917905400009', 'You are special in my heart, sweetie. Forever will you, darling.', 6, 6)
+pywhatkit.sendwhatmsg_instantly('+917905400009', 'You are special in my heart, sweetie. Forever will you, darling.')
